02f984bd0f9819a0ffd0c1a483a315560d630999
williamjohngardner <bill@williamjohngardner.com>
2016-06-14 17:49:24 -0400

    everything works and looks nice

--- calc/views.py
+++ calc/views.py
@@ -5,11 +5,17 @@ from calc.forms import Calculator
 
 def index_view(request):
     result = ""
+    a = ''
+    b = ''
+    operators = ''
     form = Calculator
     print(request.POST)
     if request.method == "POST":
         form = Calculator(request.POST)
         if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            operators = form.cleaned_data['operators']
             if form.cleaned_data['operators'] == '+':
                 result = form.cleaned_data['a'] + form.cleaned_data['b']
             elif form.cleaned_data['operators'] == '-':
@@ -18,4 +24,4 @@ def index_view(request):
                 result = form.cleaned_data['a'] * form.cleaned_data['b']
             elif form.cleaned_data['operators'] == '/':
                 result = form.cleaned_data['a'] / form.cleaned_data['b']
-    return render(request, "index.html", {"form": form, "result": result})
+    return render(request, "index.html", {"form": form, "result": result, "a": a, "b": b, "operators": operators})
